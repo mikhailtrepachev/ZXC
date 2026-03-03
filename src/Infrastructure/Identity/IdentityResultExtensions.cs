df7836dde3d7e5a0e2b8a563a8fa32f415daf55d
mikhailtrepachev <mikhail_trepachev@outlook.cz>
2026-03-03 11:50:53 +0100

    init

---
+++ src/Infrastructure/Identity/IdentityResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ZxcBank.Application.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ZxcBank.Infrastructure.Identity;
+
+public static class IdentityResultExtensions
+{
+    public static Result ToApplicationResult(this IdentityResult result)
+    {
+        return result.Succeeded
+            ? Result.Success()
+            : Result.Failure(result.Errors.Select(e => e.Description));
+    }
+}
---
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -0,0 +1,11 @@
+﻿using ZxcBank.Application.Common.Behaviours;
+using ZxcBank.Application.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace ZxcBank.Application.UnitTests.Common.Behaviours;
+
+public class RequestLoggerTests
+{
+}
