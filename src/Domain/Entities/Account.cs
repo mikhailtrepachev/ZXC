ca5ab8c0ce79e2ce52f544c1ad0332fb632a0f0f
mikhailtrepachev <mikhail_trepachev@outlook.cz>
2026-03-03 15:31:40 +0100

    Entities, fixed migrations

---
+++ src/Domain/Entities/Account.cs
@@ -0,0 +1,12 @@
+namespace ZxcBank.Domain.Entities;
+
+public class Account : BaseAuditableEntity
+{
+    public required string OwnerId { get; set; }
+
+    public required string AccountNumber { get; set; }
+    
+    public decimal Balance { get; set; }
+    
+    public bool IsFrozen { get; set; }
+}
---
+++ src/Domain/Entities/Transaction.cs
@@ -0,0 +1,14 @@
+namespace ZxcBank.Domain.Entities;
+
+public class Transaction : BaseAuditableEntity
+{
+    public required string FromAccountId { get; set; }
+    
+    public required string ToAccountId { get; set; }
+    
+    public decimal Amount { get; set; }
+    
+    public TransactionStatus Status { get; set; }
+    
+    public string? ApprovedBy { get; set; } // in case of children accounts
+}
