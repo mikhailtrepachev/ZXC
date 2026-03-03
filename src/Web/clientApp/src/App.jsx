b20ccd52865bb3c389c63df2394de6f41e3b1437
Leeeeroooyy <leeeeroooyy@gmail.com>
2026-03-03 16:58:43 +0100

    Imports fix

--- src/Web/clientApp/src/App.jsx
+++ src/Web/clientApp/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CreateAccountPage from "./pages/CreateAccountPage";
+import Header from "./widgets/Header.jsx";
+import Footer from "./widgets/Footer.jsx";
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<CreateAccountPage />} />
       </Routes>
+        <Footer />
     </BrowserRouter>
   );
 }
