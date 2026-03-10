aa0247e1db747a6a6cf04301c5812476f5c9fc08
mikhailtrepachev <mikhail_trepachev@outlook.cz>
2026-03-10 13:15:36 +0100

    rabbitmq, redis, fixed seq, loggin on login

--- src/Application/Auth/Commands/LoginCommand.cs
+++ src/Application/Auth/Commands/LoginCommand.cs
@@ -1,5 +1,6 @@
 using ZxcBank.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace ZxcBank.Application.Auth.Commands;
 
@@ -12,20 +13,27 @@ public record LoginCommand : IRequest<string>
 public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
 {
     private readonly IIdentityService _identityService;
+    
+    private readonly ILogger<LoginCommandHandler> _logger;
 
-    public LoginCommandHandler(IIdentityService identityService)
+    public LoginCommandHandler(IIdentityService identityService, ILogger<LoginCommandHandler> logger)
     {
         _identityService = identityService;
+        _logger = logger;
     }
 
     public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
-        var token = await _identityService.LoginAsync(request.Email, request.Password);
+        string? token = await _identityService.LoginAsync(request.Email, request.Password);
 
         if (token == null)
         {
+            _logger.LogInformation("Login failed: {AttemptedEmail}", request.Email);
+            
             throw new UnauthorizedAccessException("Neplatné přihlašovací údaje");
         }
+        
+        _logger.LogInformation("Login successful: {AttemptedEmail}", request.Email);
 
         return token;
     }
